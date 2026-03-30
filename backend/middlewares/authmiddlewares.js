2d61615d38e3042b7dcfb80759267cb5ffa0bf8e
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-30 18:54:43 +0545

    fix: add token validation in auth middleware

--- backend/middlewares/authmiddlewares.js
+++ backend/middlewares/authmiddlewares.js
@@ -2,6 +2,12 @@ const jwt = require('jsonwebtoken');
 const authmiddlewares = async (req, res, next)=>{
     try{
         const token = req.headers.authorization?.split(" ")[1];
+        if(!token){
+            return res.status(401).json({
+                message:"token invalid"
+            })
+        }
+        
 
     }catch(e){
 
