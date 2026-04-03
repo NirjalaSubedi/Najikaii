ae4ad629c20fec9481a99c7396b3c6980973c256
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-04-03 19:26:19 +0545

    fix: add email existence check in OTP verification

--- backend/Controllers/verifyOTP.js
+++ backend/Controllers/verifyOTP.js
@@ -1,7 +1,14 @@
 const user = require('../models/UserModels');
 exports.verifyOtp= async (req,res)=>{
     try{
+        const {email,otp} = req.body;
 
+        const verifyuser = await user.findOne({email});
+        if(!verifyuser){
+            res.status(404).json({
+                message:"email doesnot exist"
+            })
+        }
     }catch(error){
 
     }
