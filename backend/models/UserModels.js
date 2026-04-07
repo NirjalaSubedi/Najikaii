981d6605f022a07d57971252a2963110c92de5b7
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-04-07 08:17:19 +0545

    feat: add cart field to user schema for product management

--- backend/models/UserModels.js
+++ backend/models/UserModels.js
@@ -46,7 +46,12 @@ const userSchema=new mongoose.Schema({
     },
     otpExpire: {
         type: Date
-    }
+    },
+    // UserModels.js bhitra yo thapnus
+    cart: [{
+        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+        quantity: { type: Number, default: 1 }
+    }]
 }, {timestamps: true})
 
 module.exports = mongoose.model('User', userSchema);
