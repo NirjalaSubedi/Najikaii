04d718135f8fa27bd451553fc4406bfc4a69927f
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-04-07 21:17:14 +0545

    feat: implement user existence check in AddToCart function

--- backend/Controllers/cartcontrolletrs.js
+++ backend/Controllers/cartcontrolletrs.js
@@ -4,7 +4,17 @@ const productmodel = require('../models/ProductModels');
 //Add to cart logic
 exports.AddToCart = async(req,res)=>{
     try{
+        const {productid, quantity}= req.body;
+        const userId= req.user.id;
 
+        //check if user exist or not
+        const user = await usermodel.findById(userId);
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"user vetiyana"
+            })
+        }
     }catch(error){
         res.status(500).json({
             success:false,
