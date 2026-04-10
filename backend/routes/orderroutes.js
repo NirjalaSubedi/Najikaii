7e1c53ca3ed736986738d2394e50c25d0ca11c89
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-04-10 20:54:16 +0545

    feat: add route for placing orders with authentication and role authorization

--- backend/routes/orderroutes.js
+++ backend/routes/orderroutes.js
@@ -1,3 +1,8 @@
 const express =require('express');
 const router= express.Router();
 const {authmiddlewares,authorizeRoles}= require('../middlewares/authmiddlewares');
+const {PlaceOrder}=require('../Controllers/ordercontrollers');
+
+router.post('/placeorder',authmiddlewares,authorizeRoles('Customer'),PlaceOrder);
+
+module.exports=router;
