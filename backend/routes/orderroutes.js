68b073757f28b65b9a583ffeef08fe4a5fdc5937
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-04-13 21:26:10 +0545

    feat: add getorders function to order routes for viewing orders by roles

--- backend/routes/orderroutes.js
+++ backend/routes/orderroutes.js
@@ -1,8 +1,9 @@
 const express =require('express');
 const router= express.Router();
 const {authmiddlewares,authorizeRoles}= require('../middlewares/authmiddlewares');
-const {PlaceOrder}=require('../Controllers/ordercontrollers');
+const {PlaceOrder,getorders}=require('../Controllers/ordercontrollers');
 
 router.post('/placeorder',authmiddlewares,authorizeRoles('Customer'),PlaceOrder);
+router.get('/vieworders',authmiddlewares,authorizeRoles('Customer','admin','vendor'),getorders)
 
 module.exports=router;
