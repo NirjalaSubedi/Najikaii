39f253f094c7218821a7cf1d53070a6f27714226
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-04-15 22:09:50 +0545

    feat: add update and cancel order routes to order management

--- backend/routes/orderroutes.js
+++ backend/routes/orderroutes.js
@@ -1,9 +1,13 @@
 const express =require('express');
 const router= express.Router();
 const {authmiddlewares,authorizeRoles}= require('../middlewares/authmiddlewares');
-const {PlaceOrder,getorders}=require('../Controllers/ordercontrollers');
+const {PlaceOrder,getorders,UpdateOrderStatus,CancelOrder}=require('../Controllers/ordercontrollers');
 
 router.post('/placeorder',authmiddlewares,authorizeRoles('Customer'),PlaceOrder);
-router.get('/vieworders',authmiddlewares,authorizeRoles('Customer','Admin','Vendor'),getorders)
+router.get('/vieworders',authmiddlewares,authorizeRoles('Customer','Admin','Vendor'),getorders);
+// Status update ko lagi
+router.put('/update-status/:orderId', authmiddlewares, authorizeRoles('admin', 'vendor', 'Admin', 'Vendor'), UpdateOrderStatus);
 
+// Cancel ko lagi
+router.put('/cancel/:orderId', authmiddlewares, authorizeRoles('Customer', 'admin', 'Admin'), CancelOrder);
 module.exports=router;
