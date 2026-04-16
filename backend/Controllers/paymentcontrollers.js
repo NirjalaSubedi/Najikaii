64a201960b8a1532811eb4baa3800791c0d5eca9
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-04-16 21:20:30 +0545

    feat: add axios dependency and implement esewaPayment function to decode payment data

--- backend/Controllers/paymentcontrollers.js
+++ backend/Controllers/paymentcontrollers.js
@@ -1,8 +1,14 @@
 const crypto = require('crypto');
+const axios= require('axios');
+const order= require('../models/OrderModels');
+const payment= require('../models/paymentModel');
 
 exports.esewaPayment= async (req,res)=>{
     try{
+        const {data} = req.query;
 
+        //data decode gareko
+        const decoded = JSON.parse(Buffer.from(data,'base64').toString('utf-8'));
     }catch(error){
         res.status(500).json({
             success:false,
