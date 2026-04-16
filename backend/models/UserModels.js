f8b2fac61f7e482d44e4254827a0c4307afec0da
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-04-16 22:09:33 +0545

    fix: add location field with geospatial indexing to user schema

--- backend/models/UserModels.js
+++ backend/models/UserModels.js
@@ -26,6 +26,19 @@ const userSchema=new mongoose.Schema({
         city: { type: String, default: 'Jhumka' }, // Default making local area
         province: { type: String, default: 'Koshi' }
     },
+
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            default: 'Point'
+        },
+        coordinates: {
+            type: [Number], // [Longitude, Latitude]
+            default: [87.1685, 26.6500] // Default Jhumka ko coordinates
+        }
+    },
+
     isVerified: { 
         type: Boolean, 
         default: false 
@@ -53,5 +66,7 @@ const userSchema=new mongoose.Schema({
         quantity: { type: Number, default: 1 }
     }]
 }, {timestamps: true})
+
+userSchema.index({ location: "2dsphere" });
 
 module.exports = mongoose.model('User', userSchema);
