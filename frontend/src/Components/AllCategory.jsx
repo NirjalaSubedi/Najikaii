63ef80e1c88663f88f35269975c0af7188f15b8b
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-04-20 10:18:01 +0545

    feat: implement AllCategory component structure

---
+++ frontend/src/Components/AllCategory.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {LayoutGrid, Sprout, Leaf, Coffee, Utensils} from "lucide-react";
+const AllCategory=()=>{
+    return(
+        <div>
+            
+        </div>
+    )
+}
+export default AllCategory;
