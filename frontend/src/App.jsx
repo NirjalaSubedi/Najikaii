e6de511211c2ebc2573cfe7dd6e53c057f849eba
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-04-22 22:25:51 +0545

    feat: add route for AllShops component in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import AllShops from "./pages/AllShops";
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/all-shops" element={<AllShops />} />
       </Routes>
     </div>
   );
