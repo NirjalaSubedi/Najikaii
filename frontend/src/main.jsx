3930ec9bcf2da4d795e7cfa233445cb55659b2f7
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-05-15 21:15:07 +0545

    feat: wrap App component with GoogleOAuthProvider for Google authentication

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
+    </GoogleOAuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
