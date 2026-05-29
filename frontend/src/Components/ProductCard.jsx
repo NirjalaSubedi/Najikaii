0ea6c7d6357a5b20d3034f16d177bd0f4e100aec
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-05-29 13:29:41 +0545

    feat: implement ProductCard component with product details destructuring and default values

--- frontend/src/Components/ProductCard.jsx
+++ frontend/src/Components/ProductCard.jsx
@@ -1,4 +1,21 @@
-const ProductCard = ()=>{
+import React from 'react';
+import {heart,star,plus} from 'lucide-react';
+const ProductCard = ({product})=>{
+    if(!product)return null;
+
+    const {
+    name = "Unnamed Product",
+    sellingPrice = 0,      
+    actualPrice,          
+    discountPercentage,   
+    image = "https://via.placeholder.com/300",
+    unitType = "item",   
+    distance,          
+    isFastDelivery = false,
+    stock = 0,
+    vendor = {}
+  } = product;
+
     return(
         <>
         </>
