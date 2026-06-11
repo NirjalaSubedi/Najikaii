4c8f111aede67ef920b5c4bcc0d9d4a7bdbdb60f
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-06-11 20:53:44 +0545

    fix: remove unnecessary comment in UserModels schema

--- backend/models/UserModels.js
+++ backend/models/UserModels.js
@@ -66,7 +66,6 @@ const userSchema=new mongoose.Schema({
     otpExpire: {
         type: Date
     },
-    // UserModels.js bhitra yo thapnus
     cart: [{
         product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
         quantity: { type: Number, default: 1 }
