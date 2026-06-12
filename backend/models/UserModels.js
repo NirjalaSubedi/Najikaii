048f4dda77fcf24cd9ec33f346af67b77ee7a6ef
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-06-12 06:42:33 +0545

    fix: remove cart field from UserModels schema to streamline user data structure

--- backend/models/UserModels.js
+++ backend/models/UserModels.js
@@ -66,22 +66,6 @@ const userSchema=new mongoose.Schema({
     otpExpire: {
         type: Date
     },
-    
-    cart: [{
-        product: { 
-            type: mongoose.Schema.Types.ObjectId, 
-            ref: 'Product' 
-        },
-        quantity: { 
-            type: Number, 
-            default: 1 
-        },
-        price: { 
-            type: Number, 
-            required: true,
-            default: 0 
-        }
-    }],
 
     googleId: { type: String },
     facebookId: { type: String },
